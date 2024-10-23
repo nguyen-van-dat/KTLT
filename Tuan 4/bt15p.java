a5043340feef7f96de2f598772e645e329840137
Nguyễn Văn Đạt 108398 <dat108398@donga.edu.vn>
2024-10-23 17:26:23 +0700

    Add files via upload

---
+++ Tuan 4/bt15p.java
@@ -0,0 +1,13 @@
+package nguyenvandat;
+public class bt15p {
+    public static void main(String[] args) {
+        int arr[] = {2, 5, 7 ,10, 19, 27};
+        int cs = 0;
+        System.out.printf("phần tử thứ 1: arr [%d] = %d",cs,arr[cs ]);
+        System.out.printf("phần tử thứ 2: arr [%d] = %d",cs=cs+1,arr[cs]);
+        System.out.printf("phần tử thứ 3: arr [%d] = %d",cs=cs+1,arr[cs]);
+        System.out.printf("phần tử thứ 4: arr [%d] = %d",cs=cs+1,arr[cs]);
+        System.out.printf("phần tử thứ 5: arr [%d] = %d",cs=cs+1,arr[cs]);
+        System.out.printf("phần tử thứ 6: arr [%d] = %d",cs=cs+1,arr[cs]);
+        }
+}
